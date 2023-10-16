4101a41100bd3f5e56177dee8ca8ef4dff5787e5
nobleaustine <nobleaustine2001@gmail.com>
2023-10-16 08:55:19 +0200

    try 2 to deploy

--- source/index.js
+++ source/index.js
@@ -21,15 +21,15 @@ app.use((req,res,next)=>{
 })
 
 // ------------ Deployment ---------------
-// const __dirname1 = path.resolve();
-// if(process.env.NODE_ENV === 'production'){
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
 
-//    app.use(express.static(path.join(__dirname1,"/Client/build")));
-//    app.get('*',(req,res)=>{ res.sendFile(path.resolve(__dirname1,"Client","build","index.html")) })
-// }else{
-//     app.get('/',(req,res)=>{ res.send("API is running ") });
+   app.use(express.static(path.join(__dirname1,"/Client/build")));
+   app.get('*',(req,res)=>{ res.sendFile(path.resolve(__dirname1,"Client","build","index.html")) })
+}else{
+    app.get('/',(req,res)=>{ res.send("API is running ") });
 
-// }
+}
 // ------------ Deployment ---------------
 
 const PORT = process.env.PORT 
