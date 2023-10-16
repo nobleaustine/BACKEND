6ebbf732b20c5837d8be09052aadada497b7f4e1
nobleaustine <nobleaustine2001@gmail.com>
2023-10-16 10:57:28 +0200

    5 th try with a new route

--- source/routes/tureONian.js
+++ source/routes/tureONian.js
@@ -3,6 +3,12 @@ const {register,login,getMe} = require('../controller/tureONianController')
 const {protect} = require('../middleware/checkAuth')
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+    return res.status(200).json({
+      title: "BACKEND TESTING",
+      message: "The app is working properly ......",
+    });
+  });
 
 router.route('/tureONian/register').post(register);
 
